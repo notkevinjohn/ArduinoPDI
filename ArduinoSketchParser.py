4ca63e38abc1e4bac34e3f479c57d362e0928c14
Kevin John <notkevinjohn@gmail.com>
2023-11-28 12:00:10 -0800

    updated sketch parser to collect more data including name, version, and baud rate

--- ArduinoSketchParser.py
+++ ArduinoSketchParser.py
@@ -5,29 +5,57 @@ from FunctionData import *
 
 class ArduinoSketchParser():
 	adornmentPattern = "^\/\/\[(.*?)\]"
+	baudRatePattern = "^Serial.begin\((.*?)\);"
+	nameVersionPattern = "^Serial.println\(\"(.*?) (.*?)\"\);"
+	functionPattern = "^[a-zA-Z0-9]+ [a-zA-Z0-9_]+\((.*?)\)"
 	adornedFunctions = {}
 
 	def __init__(self, target):
 		self.target = target
 
 	def parse(self):
+		sketchData = SketchData()
+		currentFunction = None
 		with open (self.target) as file:
 			lines = file.readlines()
 			for i in range(0,len(lines)):
-				line = lines[i]
-				if self.checkAdornment(line):
-					adornmentKeys = self.parseAdornment(line)
-					nextLine = lines[i+1]
-					functionData = self.extractFunctionData(nextLine)
-					for key in adornmentKeys:
-						if not key in self.adornedFunctions:
-							self.adornedFunctions[key] = []
-						self.adornedFunctions[key].append(functionData)
+				line = lines[i].strip()
+				if self.checkFunctionPattern(line):
+					functionData = self.extractFunctionData(line)
+					currentFunction = functionData
+					lastLine = lines[i-1]
+					if self.checkAdornment(lastLine):
+						adornmentKeys = self.parseAdornment(lastLine)
+						for key in adornmentKeys:
+							if not key in sketchData.adornedFunctions:
+								sketchData.adornedFunctions[key] = []
+							sketchData.adornedFunctions[key].append(functionData)
 
-		return self.adornedFunctions
+				if currentFunction!=None and currentFunction.name == "setup":
+					if self.checkBaudRate(line):
+						sketchData.baudrate = self.extractBaudRate(line)
+					elif self.checkNameVersionPattern(line):
+						sketchData.name, sketchData.version = self.extractNameVersion(line)
+
+		return sketchData
+
+	def checkFunctionPattern(self, line):
+		if re.match(self.functionPattern, line):
+			return True
+		return False
+
+	def checkNameVersionPattern(self, line):
+		if re.match(self.nameVersionPattern, line):
+			return True
+		return False
 
 	def checkAdornment(self, line):
 		if re.match(self.adornmentPattern, line):
+			return True
+		return False
+
+	def checkBaudRate(self, line):
+		if re.match(self.baudRatePattern, line):
 			return True
 		return False
 
@@ -39,6 +67,17 @@ class ArduinoSketchParser():
 		adornmentKeys = [x.strip() for x in adornmentKeys]
 		return adornmentKeys
 
+	def extractBaudRate(self, line):
+		baudRate = line.split('(')[1].split(")")[0]
+		baudRate = int(baudRate)
+		return baudRate
+
+	def extractNameVersion(self, line):
+		args = line.split('"')[1]
+		args = args.split(" ")
+		name = args[0]
+		version = args[1]
+		return name, version
 
 	def extractFunctionData(self, fstring):
 		fdata = FunctionData()
