ae9558f505ac10251b332b491ffafd97e007b0ad
Kevin John <notkevinjohn@gmail.com>
2023-12-03 13:05:41 -0800

    creating interface generator

--- PreCompile.py
+++ PreCompile.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 from ArduinoSketchParser import ArduinoSketchParser
+from InterfaceGenerator import InterfaceGenerator
 from PythonClassExporter import PythonClassExporter
 
 device = "/dev/arduino-mega"
 sketchData = ArduinoSketchParser("Sketch.c").parse()
-exporter = PythonClassExporter(sketchData, device)
+interface = InterfaceGenerator(sketchData).writeFile("Interface.txt")
+#exporter = PythonClassExporter(sketchData, device)
 
 
